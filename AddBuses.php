75dbd503a61c8159c628d48c05393c7337491b39
HessaAlbahlal <135004878+Ha2s@users.noreply.github.com>
2023-10-28 18:21:19 +0300

    Update AddBuses.php

--- AddBuses.php
+++ AddBuses.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['MAILID'])) {
 <?php
     exit();
 }
-use 'dbconnection.php';
+use 'dbconnection';
 
 $stmt = $connection->prepare("select * from admin where MAILID = ?");
 $stmt->bind_param("s", $_SESSION['MAILID']);
